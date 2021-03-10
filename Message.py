1db126d6762976ee890b305dc7869ec66be1ab02
hardikkamboj <53142482+hardikkamboj@users.noreply.github.com>
2021-03-10 14:08:16 +0530

    Create Message.py

---
+++ Message.py
@@ -0,0 +1,4 @@
+whatever_you_do = "Whatever you do will be insignificant, but it is very important that you do it.\n(Mahatma Gandhi)"
+msg = Message(window, text = whatever_you_do)
+msg.config(bg='lightgreen', font=('times', 24, 'italic'))
+msg.pack()
